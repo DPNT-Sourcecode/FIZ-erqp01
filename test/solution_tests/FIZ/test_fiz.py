bb8ab621857ace6d8b26b164a7c50c965011c092
sbx_user1051 <sbx_user1051@169.254.110.109>
2019-09-22 12:53:09 +0000

    Sun Sep 22 12:53:09 UTC 2019

--- test/solution_tests/FIZ/test_fiz.py
+++ test/solution_tests/FIZ/test_fiz.py
@@ -2,10 +2,10 @@ from solutions.FIZ import fizz_buzz_solution
 
 class TestHlo2():
     def test_fiz(self):
-        assert fizz_buzz_solution.fizz_buzz(3) == 'fizz'
+        assert fizz_buzz_solution.fizz_buzz(3) == 'fizz fake deluxe'
         assert fizz_buzz_solution.fizz_buzz(12) == 'fizz'
         assert fizz_buzz_solution.fizz_buzz(10) == 'buzz'
-        assert fizz_buzz_solution.fizz_buzz(15) == 'fizz buzz'
+        assert fizz_buzz_solution.fizz_buzz(15) == 'fizz buzz fake deluxe'
         assert fizz_buzz_solution.fizz_buzz(31) == 'fizz'
         assert fizz_buzz_solution.fizz_buzz(51) == 'fizz buzz'
         assert fizz_buzz_solution.fizz_buzz(53) == 'fizz buzz'
@@ -16,4 +16,5 @@ class TestHlo2():
         
         
         
+
 
