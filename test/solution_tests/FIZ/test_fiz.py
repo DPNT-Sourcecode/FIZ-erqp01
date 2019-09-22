ce540cd49c18dc4f42a4328f6c12868621ef53b9
sbx_user1051 <sbx_user1051@169.254.236.37>
2019-09-22 08:28:48 +0000

    Sun Sep 22 08:28:48 UTC 2019

--- test/solution_tests/FIZ/test_fiz.py
+++ test/solution_tests/FIZ/test_fiz.py
@@ -1,6 +1,7 @@
-from solutions.HLO2 import hello2_solution
+from solutions.FIZ import fizz_buzz_solution
 
 class TestHlo2():
-    def test_hlo2(self):
-        assert hello2_solution.hello('Mike') == 'Hello, Mike!'
+    def test_fiz(self):
+        assert fizz_buzz_solution.fizz_buzz(3) == 'fizz'
+
 
