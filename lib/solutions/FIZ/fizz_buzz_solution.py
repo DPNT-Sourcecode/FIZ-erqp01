23aabaa0578d032181cbf23054705ec3b12fb7cd
sbx_user1051 <sbx_user1051@169.254.236.37>
2019-09-22 08:50:58 +0000

    Sun Sep 22 08:50:58 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -4,11 +4,11 @@ import math
 def fizz_buzz(number):
     mult3 = False
     mult5 = False
-    a = math.modf(number/3.0)
-    b = math.modf(number/5.0)
-    if (a[0]==0.0):
+    res3 = math.modf(number/3.0)
+    res5 = math.modf(number/5.0)
+    if (res3[0]==0.0):
         mult3 = True
-    if (b[0]==0.0):
+    if (res5[0]==0.0):
         mult5 = True
     if (mult3 and mult5):
         return('fizz buzz')
@@ -18,6 +18,7 @@ def fizz_buzz(number):
         return('buzz')
 
     return(number)
+
 
 
 
--- test/solution_tests/FIZ/test_fiz.py
+++ test/solution_tests/FIZ/test_fiz.py
@@ -3,4 +3,13 @@ from solutions.FIZ import fizz_buzz_solution
 class TestHlo2():
     def test_fiz(self):
         assert fizz_buzz_solution.fizz_buzz(3) == 'fizz'
+        assert fizz_buzz_solution.fizz_buzz(12) == 'fizz'
+        assert fizz_buzz_solution.fizz_buzz(10) == 'buzz'
+        assert fizz_buzz_solution.fizz_buzz(15) == 'fizz buzz'
+        assert fizz_buzz_solution.fizz_buzz(8) == 8
+        
+        
+        
+        
+
 
