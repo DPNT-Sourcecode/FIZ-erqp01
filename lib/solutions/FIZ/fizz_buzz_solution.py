28398bfee42214de428738d22d6dfc1cac5f8062
sbx_user1051 <sbx_user1051@169.254.236.117>
2019-09-22 11:24:23 +0000

    Sun Sep 22 11:24:23 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -4,21 +4,37 @@ import math
 def fizz_buzz(number):
     mult3 = False
     mult5 = False
+    result = ''
     res3 = math.modf(number/3.0)
     res5 = math.modf(number/5.0)
     snumber = str(number)
     in3 = '3' in snumber
     in5 = '5' in snumber
+    delx = (snumber == len(snumber) * snumber[0]) and (number > 10) #check for deluxe
+    
     if (res3[0]==0.0 or in3):
         mult3 = True
     if (res5[0]==0.0 or in5):
         mult5 = True
-    if (mult3 and mult5):
-        return('fizz buzz')
-    if (mult3):
-        return('fizz')
-    if (mult5):
-        return('buzz')
 
-    return(number)
+    if (mult3):
+        result = 'fizz'
+    if (mult5):
+        result = 'buzz'
+    if (mult3 and mult5):
+        result = 'fizz buzz'
+    
+    if (delx):
+        if result = '':
+            result = 'deluxe'
+        else:
+            result = result + 'deluxe'
+    
+    if result = '':
+        return(number)
+    else:
+        return(result)
+
+r1 = fizz_buzz('15')
+r2 = fizz_buzz('555')
 
