803836b6005d5360872e672eddf05f9be1849e2d
sbx_user1051 <sbx_user1051@169.254.236.37>
2019-09-22 08:43:48 +0000

    Sun Sep 22 08:43:48 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,3 +1,17 @@
 # noinspection PyUnusedLocal
+import math
+
 def fizz_buzz(number):
-    raise NotImplementedError()
+    mult3 = False
+    mult5 = False
+    a = math.modf(number/3.0)
+    b = math.modf(number/5.0)
+    if (a[0]==0.0) mult3 = True
+    if (b[0]==0.0) mult5 = True
+    return (number)
+
+fizz_buzz(3)
+fizz_buzz(15)
+fizz_buzz(4)
+
+
